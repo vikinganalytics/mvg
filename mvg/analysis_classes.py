4657188de50d012c6a00bc9064a2410bd4d9a09c
Fredrik Wartenberg <fredrik.wartenberg@vikinganalytics.se>
2021-04-15 08:03:00 +0200

    RMS analysis class with new result structure

--- mvg/analysis_classes.py
+++ mvg/analysis_classes.py
@@ -2,20 +2,21 @@ import numpy as np
 import pickle
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-
+import datetime
 from mvg import plotting
+
 
 # Base class for 
 class Analysis:
-
     # Init class with results
     def __init__(self, results, t_zone="Europe/Stockholm", t_unit="ms"):
+        _ = results  # TODO
         self.feature = None
         self.request_id = "0ABC"
         self.t_zone = t_zone
         self.t_unit = t_unit
         self.dframe = None
+        self.time_column = "timestamps"
 
     # Summarizes result
     def summary(self):
@@ -40,36 +41,42 @@ class Analysis:
         pickle.dump(self, filehandler)
         filehandler.close()
 
-    def _fix_ts(self):
+    def _add_datetime(self):
+        breakpoint()
         if self.t_zone is not None:
-            self.dframe = self.dframe.assign(
-                timestamps=pd.to_datetime(
-                    self.dframe["timestamps"],
-                    unit=self.t_unit
-                )
-            )
+            self.dframe = self.dframe.assign(datetime=pd.to_datetime(
+                self.dframe["timestamps"],
+                unit=self.t_unit,
+                utc=True))
+            
+            self.dframe = self.dframe.assign(datetime = (
+                self.dframe['datetime'].
+                dt.tz_convert(self.t_zone)
+            ))
+            self.time_column = "datetime"
+
 
 
 class RMS(Analysis):
 
-    def __init__(self, results, t_zone="Europe/Stockholm", t_unit="ms"):
+    def __init__(self, results, t_zone = "Europe/Stockholm", t_unit = "ms"):
         Analysis.__init__(self, results, t_zone, t_unit)
-        self.result = results['result']
-        self.success = results['success']
-        self.dframe = pd.DataFrame.from_dict(self.result)
-        self._fix_ts()
-
+        breakpoint()
+        self.result=results['results']
+        self.status=results['status']
+        self.dframe=pd.DataFrame.from_dict(self.result)
+        self._add_datetime()
 
     def summary(self):
         print(f"Summary for {self.request_id}")
-        from_t = self.dframe["timestamps"].min()
-        to_t = self.dframe["timestamps"].max()
+        from_t=self.dframe[self.time_column].min()
+        to_t=self.dframe[self.time_column].max()
         print(f"Time period from {from_t} to {to_t}")
         print(self.dframe.describe())
 
     def plot(self):
         print("Plot")
-        self.dframe.plot(x="timestamps", y=["rms", "dc", "utilization"])
+        self.dframe.plot(x = self.time_column, y = ["rms", "dc", "utilization"])
         plt.title(f"RMS Summary plot for request {self.request_id}")
         plt.show()
 
@@ -85,13 +92,15 @@ class ModeId(Analysis):
         self.result = results['results']
         self.success = results['status']
         self.dframe = pd.DataFrame.from_dict(self.result)
-        self._fix_ts()
+        self._add_datetime()
 
     def summary(self):
+        # Header
         print(f"Summary for {self.request_id}")
         from_t = self.dframe["timestamps"].min()
         to_t = self.dframe["timestamps"].max()
         print(f"Time period from {from_t} to {to_t}")
+
         # labels
         tbl = self.dframe.groupby(['labels']).agg(np.size)
         tbl['uncertain'] = tbl['uncertain']/sum(tbl['uncertain'])*100
@@ -99,6 +108,8 @@ class ModeId(Analysis):
         print()
         print("Labels")
         print(tbl)
+
+        # Uncertain
         tbl2 = self.dframe.groupby(['labels','uncertain'],).agg(np.size)
         tbl2["counts"] = tbl2["timestamps"]
         tbl2 = tbl2.rename(columns={"timestamps": "portion"})
@@ -107,29 +118,13 @@ class ModeId(Analysis):
         print("Lables & uncertain labels")
         print(tbl2)
 
-
-    def plot(self):
-        print("Plot")
-        df_plt = self.dframe
-        df_plt["uncertain"] = pd.Categorical(self.dframe.uncertain).codes
-        #self.dframe.plot(x="timestamps", y=["labels", "uncertain"])
-        # self.dframe["uncert"] = 
-        sns.scatterplot(x=df_plt["timestamps"],
-                        y=df_plt["labels"],
-                        hue=df_plt["uncertain"])
-
-        plt.title(f"ModeId Summary plot for request {self.request_id}")
-        plt.xticks(rotation=30, horizontalalignment='right')
-        plt.show()
-
     def s_plot(self):
         print("Plot")
-        breakpoint()
-        img = plotting.modes_over_time(self.dframe,
-                                       self.request_id)
-        print("Plotting")
-        
+        plotting.modes_over_time(self.dframe,
+                                 self.request_id,
+                                 timeunit=self.t_unit)
+        plt.show()
+
     def to_df(self):
         print("To df")
         return self.dframe
-
