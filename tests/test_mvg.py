6e6b02635dbd02dd6b873b29c0d7cdf5420cd817
OskarLiew <oskar@liew.se>
2021-07-28 14:23:50 +0200

    Fixing tests

--- tests/test_mvg.py
+++ tests/test_mvg.py
@@ -3,12 +3,12 @@ Tests in this file shall test functionlity
 not relying on access to vibium-cloud API
 """
 
-from requests import HTTPError
 import pytest
+import requests
 from mvg import MVG
 
 
 def test_create_session_without_server():
     """We should get an HTTPError because we cannot connect to localhost:8000"""
-    with pytest.raises(HTTPError):
+    with pytest.raises(requests.ConnectionError):
         MVG("localhost:8000", "NO TOKEN")
