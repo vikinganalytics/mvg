927dd5bbfec79a2b49aebe183817cdc6ccf5b051
Mohsen Nosratinia <mohsen.nosratinia@vikinganalytics.se>
2021-08-24 11:26:44 +0200

    Update setup.py
    limit module search to "mvg" folder

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/vikinganalytics/mvg",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(where="mvg"),
     license="LICENSE",
     classifiers=[
         "Programming Language :: Python :: 3",
