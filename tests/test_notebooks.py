d0d070bc05a948f86ec287e8f9af8ce623c21d4a
Mohsen Nosratinia <mohsen.nosratinia@vikinganalytics.se>
2022-02-17 11:45:42 +0100

    move notebook tests to a separate job in ci workflow

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -1,5 +1,5 @@
-import papermill as pm
 from pathlib import Path
+import papermill as pm
 
 
 def test_notebook0(vibium):
@@ -7,4 +7,4 @@ def test_notebook0(vibium):
         Path(__file__).parents[1] / "docs" / "source" / "content" / "examples"
     )
     for nbook in sorted([f for f in notebooks_path.glob("*.ipynb") if f.is_file()]):
-        pm.execute_notebook(nbook, nbook, parameters=dict(ENDPOINT=vibium))
+        pm.execute_notebook(nbook, nbook, parameters={"ENDPOINT": vibium})
