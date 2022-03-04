d020e691a1884b0339ce484e1bf2b78555f8c5e6
SergioMDCB <61144290+SergioMDCB@users.noreply.github.com>
2022-03-04 12:51:50 +0100

    Update api version

--- mvg/mvg.py
+++ mvg/mvg.py
@@ -56,7 +56,7 @@ class MVGAPI:
         self.token = token
 
         self.mvg_version = self.parse_version("v0.12.2")
-        self.tested_api_version = self.parse_version("v0.3.2")
+        self.tested_api_version = self.parse_version("v0.3.3")
 
         # Get API version
         try:
