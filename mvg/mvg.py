8e042f54e2106ba922abc8c1d9c82d9ec1788b8a
Mohsen Nosratinia <mohsen.nosratinia@vikinganalytics.se>
2022-04-25 11:31:03 +0200

    update api and mvg versions

--- mvg/mvg.py
+++ mvg/mvg.py
@@ -55,8 +55,8 @@ class MVGAPI:
         self.endpoint = endpoint
         self.token = token
 
-        self.mvg_version = self.parse_version("v0.12.3")
-        self.tested_api_version = self.parse_version("v0.3.4")
+        self.mvg_version = self.parse_version("v0.13.0")
+        self.tested_api_version = self.parse_version("v0.4.0")
 
         # Get API version
         try:
