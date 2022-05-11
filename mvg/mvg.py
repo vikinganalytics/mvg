e6741a61ca2bbdfd2e3b8d3c20463e59582b6c04
Mohsen Nosratinia <mohsen.nosratinia@vikinganalytics.se>
2022-05-11 17:19:47 +0200

    update prod version

--- mvg/mvg.py
+++ mvg/mvg.py
@@ -57,7 +57,7 @@ class MVGAPI:
         self.token = token
 
         self.mvg_version = self.parse_version("v0.13.1")
-        self.tested_api_version = self.parse_version("v0.4.0")
+        self.tested_api_version = self.parse_version("v0.4.1")
 
         # Get API version
         try:
