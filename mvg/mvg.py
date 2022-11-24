d5bf31e9f9d93a9e8843cb01646c8da3507e9e05
Vishnu Nadhan <nadhan.vishnu@gmail.com>
2022-11-24 15:01:22 +0100

    Bump minor version

--- mvg/mvg.py
+++ mvg/mvg.py
@@ -56,7 +56,7 @@ class MVGAPI:
         self.endpoint = endpoint
         self.token = token
 
-        self.mvg_version = self.parse_version("v0.13.2")
+        self.mvg_version = self.parse_version("v0.14.0")
         self.tested_api_version = self.parse_version("v0.4.7")
 
         # Get API version
