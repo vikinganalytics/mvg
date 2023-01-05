0b121e2722e1f3860e3b483d94dd5da2faccf779
Mohsen Nosratinia <mohsen.nosratinia@vikinganalytics.se>
2023-01-05 18:34:40 +0100

    bump versions

--- mvg/mvg.py
+++ mvg/mvg.py
@@ -56,8 +56,8 @@ class MVGAPI:
         self.endpoint = endpoint
         self.token = token
 
-        self.mvg_version = self.parse_version("v0.14.0")
-        self.tested_api_version = self.parse_version("v0.5.0")
+        self.mvg_version = self.parse_version("v0.14.1")
+        self.tested_api_version = self.parse_version("v0.5.1")
 
         # Get API version
         try:
