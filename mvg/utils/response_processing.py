40bec3551a8fb76cd05c41ce7eff17b4291e12fc
Vishnu Nadhan <nadhan.vishnu@gmail.com>
2023-02-08 20:28:37 +0530

    Ability to retrieve paginated analysis results

--- mvg/utils/response_processing.py
+++ mvg/utils/response_processing.py
@@ -55,3 +55,54 @@ def get_paginated_items(request: Callable, url: str, params: Dict) -> Dict:
             response = request("get", url, params=params)
             all_items += response.json()["items"]
     return {"items": all_items, "total": num_items}
+
+
+def get_paginated_analysis_results(request: Callable, url: str, params: Dict) -> Dict:
+    """
+    Iteratively construct the results of an analysis making use of the
+    pagination parameters provided in the params dictionary
+
+    Parameters
+    ----------
+    request : Callable
+        the request object
+    url : str
+        the URL to retrieve the analysis results
+    params : Dict
+        parameters dict for the request object
+
+    Returns
+    -------
+    Dict
+        Analysis results
+    """
+    # Pagination Model fields
+    paginator_model_fields = ["total", "limit", "offset"]
+    # Fields without paginated data
+    non_paginated_fields = ["mode_info", "mode_probabilities"]
+
+    response = request("get", url, params=params).json()
+    if not ("limit" in params or "offset" in params):
+        # User has not requested a subset of results
+        results = response["results"]
+
+        # Does the analysis results include pagination?
+        # We remove this check when the backend has enabled pagination for
+        # the results of all analysis
+        is_paginated = all(f in results.keys() for f in paginator_model_fields)
+        if is_paginated:
+            num_items = results["total"]
+            limit = results["limit"]
+            num_reqs = (num_items - 1) // limit
+            for idx in range(1, num_reqs + 1):
+                offset = idx * limit
+                params["offset"] = offset
+                _results = request("get", url, params=params).json()["results"]
+                for key in _results:
+                    if key not in paginator_model_fields + non_paginated_fields:
+                        results[key] += _results[key]
+
+        # Construct the response to return
+        response["results"] = results
+
+    return response
