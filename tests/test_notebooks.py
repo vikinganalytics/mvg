8adea62f25d3f148f211c7ad467372c3b216fc21
RomaR <raman.rabetski@gmail.com>
2023-09-19 16:36:06 +0200

    Temporary disabled notebook tests.

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -1,7 +1,12 @@
+import pytest
 from pathlib import Path
 import papermill as pm
 
 
+@pytest.mark.skip(
+    reason="Notebooks rely depend on data from, which needs to be fixed "
+    "(https://github.com/vikinganalytics/va-data-charlie)."
+)
 def test_notebooks(vibium):
     notebooks_path = (
         Path(__file__).parents[1] / "docs" / "source" / "content" / "examples"
